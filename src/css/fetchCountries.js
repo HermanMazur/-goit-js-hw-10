4cc0ba3569ae043d9b94c1a76dca3c118b517974
color <95304946+HermanMazur@users.noreply.github.com>
2022-08-17 09:38:15 +0300

    export function fetchCountries(name)

--- src/css/fetchCountries.js
+++ src/css/fetchCountries.js
@@ -1,13 +1,15 @@
-
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 export function fetchCountries(name) {
-    const searchCountry = 'name,official,population,flags,languages';
+    const searchCountry = 'name, capital, population, flags, languages';
     return fetch(
-        'https://restcountries.com/v3.1/name/${name}?fields=${searchCountry}'
+        `https://restcountries.com/v3.1/name/${name}?fields=${searchCountry}`
     ).then(response => {
         if (!response.ok) {
-            throw new Error
-
-        }
-    })
+            throw new Error(
+                Notify.failure('Oops, there is no country with that name')
+            );
+}
+return response.json();
+    });
 }
